97b53d38702e9834874f0b28a3f770d2e361897d
Pranav Prakash <pranny@gmail.com>
2016-07-27 13:16:57 +0530

    Version Bump

--- octo-core/bin/clean_build.sh
+++ octo-core/bin/clean_build.sh
@@ -1,2 +1,2 @@
 rm *.gem
-gem build octocore.gemspec && gem uninstall octocore --force && gem install octocore-0.0.2.gem
+gem build octocore.gemspec && gem uninstall octocore --force
--- octo-core/lib/octocore/version.rb
+++ octo-core/lib/octocore/version.rb
@@ -1,4 +1,4 @@
 module Octo
   # The current version of the library
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
 end
